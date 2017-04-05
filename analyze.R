16507b899847927fe3b96e0fd48aefdaf14c5cb0
Shohei Aoki <shoaok@gmail.com>
2017-04-06 08:41:54 +0900

    add require statement

--- analyze.R
+++ analyze.R
@@ -1,9 +1,10 @@
- df<-read.table("temp.csv",sep=",")
- names(df)<-c("i","temperature")
- ggplot(df,aes(x=temperature))+geom_density()+geom_bar()
- sd(df$temperature)
- df2<-data.frame(temperature=rnorm(20,36.6,sd=0.1))
+library(ggplot2)
+df<-read.table("temp.csv",sep=",")
+names(df)<-c("i","temperature")
+ggplot(df,aes(x=temperature))+geom_density()+geom_bar()
+sd(df$temperature)
+df2<-data.frame(temperature=rnorm(20,36.6,sd=0.1))
 
- ggplot() +
- geom_density(data=df, aes(x=temperature), color='green') +
- geom_density(data=df2, aes(x=temperature), color='red')
+ggplot() +
+  geom_density(data=df, aes(x=temperature), color='green') +
+  geom_density(data=df2, aes(x=temperature), color='red')

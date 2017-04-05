2ee253bf830f60af70d8f276ae34944278844a55
Shohei Aoki <shoaok@gmail.com>
2017-04-06 08:55:34 +0900

    update image

--- analyze.R
+++ analyze.R
@@ -1,11 +1,23 @@
 library(ggplot2)
 df<-read.table("temp.csv",sep=",")
 names(df)<-c("i","temperature")
-ggplot(df,aes(x=temperature))+geom_density()+geom_bar()
+# ggplot(df,aes(x=temperature))+geom_density()+geom_bar()
 sd(df$temperature)
-df2<-data.frame(temperature=rnorm(20,36.6,sd=0.1))
+df2<-data.frame(temperature=rnorm(200,36.6,sd=0.1))
 
 ggplot() +
+  geom_bar(data=df,aes(x=temperature)) +
   geom_density(data=df, aes(x=temperature), color='green') +
   geom_density(data=df2, aes(x=temperature), color='red')
-a <- readLines("stdin",n=1)
+
+pause = function()
+{
+    if (interactive()) {
+        invisible(readline(prompt = "Press <Enter> to continue..."))
+    }
+    else {
+        cat("Press <Enter> to continue...")
+        invisible(readLines(file("stdin"), 1))
+    }
+}
+pause()
